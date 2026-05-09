adbe210dcbcd41fb45d98403fc0eb21968cf87af
Evandeilson 'Lobão' Silva <esilva.code@outlook.com>
2026-05-09 14:40:45 -0300

    Update Discord webhook and logo URL in configuration

--- shared/config.lua
+++ shared/config.lua
@@ -1,12 +1,12 @@
 Config = {}
 
 Config.Webhooks = {
-    resources = 'YOUR_DISCORD_WEBHOOK'
+    resources = 'https://discord.com/api/webhooks/1502723129385025577/B9lQx0k4WORdwbcajdW19BNJ2FjIEc061xa7n8IP7ojt5sScZBazk8tKP8cxOpi4sUtZ'
 }
 
 Config.Bot = {
     name = 'Zorvax Logs',
-    logo = 'YOUR_LOGO_URL'
+    logo = 'https://cdn.discordapp.com/attachments/1502061515216523345/1502061770347384974/LogoOficial.png?ex=6a0051c9&is=69ff0049&hm=145c1ce06e6eb4bed635361f86645bf4e73b59485fe7b28531d8d8ef93bfd5da&'
 }
 
 Config.Colors = {
